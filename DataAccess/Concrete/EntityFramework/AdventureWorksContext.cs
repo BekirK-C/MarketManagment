3ca561af6dda741f0f15f15a54bf59c1d881a8f2
BekirK-C <b.b.kamac@gmail.com>
2022-12-02 14:35:09 +0300

    Entity, Dal, ve Bus. katmanı temel işlemler

---
+++ DataAccess/Concrete/EntityFramework/AdventureWorksContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AdventureWorksContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-MP6HDBU;Database=MarketManagment; Trusted_Connections=True");
+        }
+    }
+}
