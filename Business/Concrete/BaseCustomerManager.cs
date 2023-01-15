9e88d791432edc1ba9e76a56c3439421e540d646
BekirK-C <b.b.kamac@gmail.com>
2023-01-15 21:16:45 +0300

    Fluent Validation Implementasyonu Validation Tool ile Validate işlemi

--- Business/Concrete/BaseCustomerManager.cs
+++ Business/Concrete/BaseCustomerManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.BusinessAspects.Autofac;
+using Business.Validation.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,10 @@ namespace Business.Concrete
             _customerDal = customerDal;
         }
 
-        [SecuredOperation("admin")]
+        //[SecuredOperation("admin")]
         public virtual IResult Add(Customer customer)
         {
+            ValidationTool.Validate(new CustomerValidator(), customer);
             _customerDal.Add(customer);
             return new SuccessResult("Başarıyla Eklendi");
         }
---
+++ Business/Validation/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validation.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.FirstName).NotEmpty();
+            RuleFor(c => c.FirstName).MinimumLength(3);
+            RuleFor(c => c.LastName).MinimumLength(3);
+            RuleFor(c => c.NationalityId).Length(11);
+        }
+    }
+}
